558ad401db85b4c1827be4c4c9fd06ac4393f16d
Evgeniya Tveritinova <evgetv28@yandex.ru>
2017-11-03 16:03:40 +0300

    add artists

--- assets/js/main.js
+++ assets/js/main.js
@@ -151,6 +151,14 @@
 			    $(".slider").diyslider("move", "forth");
 			});
 
+
+			var cards = $(".artist_profile");
+			for(var i = 0; i < cards.length; i++){
+			    var target = Math.floor(Math.random() * cards.length -1) + 1;
+			    var target2 = Math.floor(Math.random() * cards.length -1) +1;
+			    cards.eq(target).before(cards.eq(target2));
+			}
+
 	});
 
 })(jQuery);
